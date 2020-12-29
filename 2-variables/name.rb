8cb5ccc4ecc45f4b9cf81fc330ebf0cdd748c3ae
jensenrex <jensenrex@gmail.com>
2020-12-30 00:22:50 +1100

    variables chapter excercises

---
+++ 2-variables/name.rb
@@ -0,0 +1,13 @@
+puts "what is your name? "
+name = gets.chomp 
+puts "hello " + name + "! You are a cool cat!"
+
+10.times do 
+  puts name
+end 
+
+puts "what is your first name? "
+first_name = gets.chomp 
+puts "what is your last name? "
+last_name = gets.chomp 
+puts first_name + " " + last_name
